8713b072cf392f40bfe81841728b1b7f67c43ead
Muhammad Ibnu Sina <m.ibnusina339@gmail.com>
2026-07-21 10:04:53 +0800

    Player Move & Camera Temporary

---
+++ Babulu City/Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float movespeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = movement * movespeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+}
